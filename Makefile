d95f8b154d27fe634affe2bef0dd13013b501ab7
wangxian <xian366@126.com>
2020-03-25 00:58:27 +0800

    更新：Makefile和startup.sh 配合管理启动服务

---
+++ Makefile
@@ -0,0 +1,103 @@
+# 项目编译与打包
+# 注意：Makefile内的shell和正常shell有区别
+# 千万注意 Makefile 格式必须用Tab，不能用space
+
+# 这里可替换为你自己的执行程序，其他代码无需更改
+APPLICATION=myapp
+PROFILES_ACTIVE=local
+
+# 项目实际运行目录
+TARGET_DIR=../${APPLICATION}_target
+
+# .PHONY是一个伪目标，可以有效防止在Makefile文件中定义的可执行命令的目标规则
+# 和工作目录下的实际文件出现名称冲突，并提高了执行Makefile的性能。
+.PHONY: clean package
+
+all:
+	@echo "USAGE: make 命令名称"
+	@echo "可选命令：build | rebuild | clean | start | stop | status | restart"
+
+build: git_update clean package
+	@echo " "
+	@echo "===================================="
+	@echo "=> ::build:: 任务执行完成"
+
+rebuild: build restart
+	@echo " "
+	@echo "===================================="
+	@echo "=> ::rebuild:: 任务执行完成"
+
+git_update:
+	git pull
+
+	@echo " "
+	@echo "---------------"
+	@echo "=> ::git_update:: 任务执行完成"
+
+clean:
+	mvn clean
+
+	@echo " "
+	@echo "---------------"
+	@echo "=> ::clean:: 任务执行完成"
+
+package:
+	@echo " "
+
+	@# 打包 - 这里是不输出的注释
+	mvn package -P ${PROFILES_ACTIVE}
+	@echo ">>> APPLICATION JAR构建完成 ...... OK"
+	@echo " "
+
+	@echo ">>> 创建 ${TARGET_DIR} 目录 ..."
+	@if [ ! -e ${TARGET_DIR} ]; then\
+		mkdir ${TARGET_DIR};\
+		mkdir ${TARGET_DIR}/logs;\
+		echo "     ......目录创建，OK";\
+	else \
+		echo "     ......目录已存在，SKIP";\
+	fi \
+
+	@echo " "
+
+	@echo ">>> 正在清理旧的编译文件..."
+	cd ${TARGET_DIR}; rm -rfv `ls |grep -v "logs"`;
+	@echo ">>> 清理旧的编译文件 ...OK"
+	@echo " "
+
+	@echo ">>>>>>>>> 正在部署到指定目录..."
+	@cp -rfv target/${APPLICATION}-server-assembly/myapp-server/* ${TARGET_DIR}/
+	@chmod 755 ${TARGET_DIR}/bin/*.sh
+	@echo ">>> 部署到指定目录 ... OK"
+
+	@echo " "
+	@echo "---------------"
+	@echo "=> ::package:: 任务执行完成"
+
+status:
+	@sh ${TARGET_DIR}/bin/startup.sh status
+
+	@echo " "
+	@echo "---------------"
+	@echo "=> ::status:: 任务执行完成"
+
+restart:
+	@sh ${TARGET_DIR}/bin/startup.sh restart
+
+	@echo " "
+	@echo "---------------"
+	@echo "=> ::restart:: 任务执行完成"
+
+start:
+	@sh ${TARGET_DIR}/bin/startup.sh start
+
+	@echo " "
+	@echo "---------------"
+	@echo "=> ::start:: 任务执行完成"
+
+stop:
+	@sh ${TARGET_DIR}/bin/startup.sh stop
+
+	@echo " "
+	@echo "---------------"
+	@echo "=> ::stop:: 任务执行完成"
--- src/bin/startup.sh
+++ src/bin/startup.sh
@@ -1,88 +1,47 @@
-#! /bin/shell
+#!/bin/bash
 
-# Copyright 2019-2029 geekidea(https://github.com/geekidea)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# 这里可替换为你自己的执行程序，其他代码无需更改
+APPLICATION=myapp
+PROFILES_ACTIVE=local
 
-#======================================================================
-# 项目启动shell脚本
-# config目录: 配置文件目录
-# logs目录: 项目运行日志目录
-# logs/spring-boot-plus_startup.log: 记录启动日志
-# logs/back目录: 项目运行日志备份目录
-# nohup后台运行
-#
-# author: geekidea
-# date: 2018-12-2
-#======================================================================
-
-# 项目名称
-APPLICATION="spring-boot-plus"
-
-# 项目启动jar包名称
-APPLICATION_JAR="${APPLICATION}.jar"
-
-# bin目录绝对路径
-BIN_PATH=$(cd `dirname $0`; pwd)
+# =================================
 # 进入bin目录
 cd `dirname $0`
+
 # 返回到上一级项目根目录路径
 cd ..
-# 打印项目根目录绝对路径
+
 # `pwd` 执行系统命令并获得结果
 BASE_PATH=`pwd`
 
-# 外部配置文件绝对目录,如果是目录需要/结尾，也可以直接指定文件
-# 如果指定的是目录,spring则会读取目录中的所有配置文件
-CONFIG_DIR=${BASE_PATH}"/config/"
-
 # 项目日志输出绝对路径
-LOG_DIR=${BASE_PATH}"/logs"
-LOG_FILE="${APPLICATION}.log"
-LOG_PATH="${LOG_DIR}/${LOG_FILE}"
-# 日志备份目录
-LOG_BACK_DIR="${LOG_DIR}/back/"
-
-# 项目启动日志输出绝对路径
-LOG_STARTUP_PATH="${LOG_DIR}/${APPLICATION}_startup.log"
-
-# 当前时间
-NOW=$(date --date='0 days ago' "+%Y-%m-%d-%H-%M-%S")
-NOW_PRETTY=$(date --date='0 days ago' "+%Y-%m-%d %H:%M:%S")
-
-# 启动日志
-STARTUP_LOG="================================================ ${NOW_PRETTY} ================================================\n"
+LOG_PATH="${BASE_PATH}/logs/app.log"
 
 # 如果logs文件夹不存在,则创建文件夹
-if [ ! -d "${LOG_DIR}" ]; then
-  mkdir "${LOG_DIR}"
+if [ ! -d `dirname ${LOG_PATH}` ]; then
+  mkdir `dirname ${LOG_PATH}`
 fi
 
-# 如果logs/back文件夹不存在,则创建文件夹
-if [ ! -d "${LOG_BACK_DIR}" ]; then
-  mkdir "${LOG_BACK_DIR}"
-fi
+# 当前时间
+NOW=`date "+%Y-%m-%d-%H-%M-%S"`
+NOW_PRETTY=`date "+%Y-%m-%d %H:%M:%S"`
 
-# 如果项目运行日志存在,则重命名备份
-if [ -f "${LOG_PATH}" ]; then
-	mv ${LOG_PATH} "${LOG_BACK_DIR}/${APPLICATION}_back_${NOW}.log"
-fi
+# 使用说明，用来提示输入参数
+usage() {
+    echo ">>> Usage: sh 脚本名.sh [start|stop|restart|status]"
+    exit 1
+}
 
-# 创建新的项目运行日志
-echo "" > ${LOG_PATH}
-
-# 如果项目启动日志不存在,则创建,否则追加
-echo ${STARTUP_LOG} >> ${LOG_STARTUP_PATH}
+# 检查程序是否在运行
+is_exist() {
+    pid=`ps -ef|grep $APPLICATION.jar|grep -v grep|awk '{print $2}' `
+    # 如果不存在返回1，存在返回0
+    if [ -z "${pid}" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
 
 #==========================================================================================
 # JVM Configuration
@@ -94,46 +53,118 @@ echo ${STARTUP_LOG} >> ${LOG_STARTUP_PATH}
 # -XX:MaxMetaspaceSize=320m:限制Metaspace增长的上限，防止因为某些情况导致Metaspace无限的使用本地内存，影响到其他程序
 # -XX:-OmitStackTraceInFastThrow:解决重复异常不打印堆栈信息问题
 #==========================================================================================
-JAVA_OPT="-server -Xms1g -Xmx1g -Xmn512m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=256m"
+JAVA_OPT="-server -Xms512m -Xmx512m -Xmn256m -XX:MetaspaceSize=32m -XX:MaxMetaspaceSize=128m"
 JAVA_OPT="${JAVA_OPT} -XX:-OmitStackTraceInFastThrow"
 
-#=======================================================
-# 将命令启动相关日志追加到日志文件
-#=======================================================
+# 启动方法
+start() {
+    is_exist
+    if [ $? -eq "0" ]; then
+        echo ">>> ${APPLICATION} is already running. pid=${pid}"
+    else
+        # 启动日志
+        STARTUP_LOG="================================================ ${NOW_PRETTY} ================================================\n"
 
-# 输出项目名称
-STARTUP_LOG="${STARTUP_LOG}application name: ${APPLICATION}\n"
-# 输出jar包名称
-STARTUP_LOG="${STARTUP_LOG}application jar name: ${APPLICATION_JAR}\n"
-# 输出项目bin路径
-STARTUP_LOG="${STARTUP_LOG}application bin  path: ${BIN_PATH}\n"
-# 输出项目根目录
-STARTUP_LOG="${STARTUP_LOG}application root path: ${BASE_PATH}\n"
-# 打印日志路径
-STARTUP_LOG="${STARTUP_LOG}application log  path: ${LOG_PATH}\n"
-# 打印JVM配置
-STARTUP_LOG="${STARTUP_LOG}application JAVA_OPT : ${JAVA_OPT}\n"
+        #=======================================================
+        # 将命令启动相关日志追加到日志文件
+        #=======================================================
+
+        # 输出项目名称
+        STARTUP_LOG="${STARTUP_LOG}>>> Application name: ${APPLICATION}\n"
+
+        # 输出jar包名称
+        STARTUP_LOG="${STARTUP_LOG}>>> Application jar name: ${APPLICATION}.jar\n"
+
+        # 输出项目根目录
+        STARTUP_LOG="${STARTUP_LOG}>>> Application root path: ${BASE_PATH}\n"
+
+        # 打印日志路径
+        STARTUP_LOG="${STARTUP_LOG}>>> Application log path: ${LOG_PATH}\n"
+
+        # 打印JVM配置
+        STARTUP_LOG="${STARTUP_LOG}>>> Application JAVA_OPT: ${JAVA_OPT}\n"
+
+        # 打印启动命令
+        STARTUP_LOG="${STARTUP_LOG}>>> Application background startup command: nohup java ${JAVA_OPT} -jar ${BASE_PATH}/lib/${APPLICATION}.jar --spring.profiles.active=${PROFILES_ACTIVE} --spring.config.location=${BASE_PATH}/config/ > ${LOG_PATH} 2>&1 &\n"
 
 
-# 打印启动命令
-STARTUP_LOG="${STARTUP_LOG}application background startup command: nohup java ${JAVA_OPT} -jar ${BASE_PATH}/lib/${APPLICATION_JAR} --spring.config.location=${CONFIG_DIR} --logging.config=${CONFIG_DIR}logback.xml > ${LOG_PATH} 2>&1 &\n"
+        #======================================================================
+        # 执行启动命令：后台启动项目,并将日志输出到项目根目录下的logs文件夹下
+        #======================================================================
+        #nohup java ${JAVA_OPT} -jar ${BASE_PATH}/lib/${APPLICATION}.jar --spring.profiles.active=${PROFILES_ACTIVE} --spring.config.location=${BASE_PATH}/config/ > ${LOG_PATH} 2>&1 &
+        nohup java ${JAVA_OPT} -jar ${BASE_PATH}/lib/${APPLICATION}.jar --spring.profiles.active=${PROFILES_ACTIVE} --spring.config.location=${BASE_PATH}/config/ > /dev/null 2>&1 &
 
+        # 进程ID
+        PID=$(ps -ef | grep ${APPLICATION}.jar | grep -v grep | awk '{ print $2 }')
+        STARTUP_LOG="${STARTUP_LOG}>>> Application pid: ${PID}\n"
 
-#======================================================================
-# 执行启动命令：后台启动项目,并将日志输出到项目根目录下的logs文件夹下
-#======================================================================
-nohup java ${JAVA_OPT} -jar ${BASE_PATH}/lib/${APPLICATION_JAR} --spring.config.location=${CONFIG_DIR} --logging.config=${CONFIG_DIR}logback.xml > ${LOG_PATH} 2>&1 &
+        STARTUP_LOG="${STARTUP_LOG}================================================ ${NOW_PRETTY} ================================================\n"
 
+        # 打印启动日志
+        echo -e ${STARTUP_LOG}
 
-# 进程ID
-PID=$(ps -ef | grep ${APPLICATION_JAR} | grep -v grep | awk '{ print $2 }')
-STARTUP_LOG="${STARTUP_LOG}application pid: ${PID}\n"
+        # 打印项目日志
+        #tail -F ${LOG_PATH}
+    fi
+}
 
-# 启动日志追加到启动日志文件中
-echo -e ${STARTUP_LOG} >> ${LOG_STARTUP_PATH}
-# 打印启动日志
-echo -e ${STARTUP_LOG}
+# 停止方法
+stop(){
+    is_exist
+    if [ $? -eq "0" ]; then
+        kill -9 $pid
+        echo ">>> ${APPLICATION} is stopping ..."
+        sleep 1
+        echo "."
+        sleep 1
+        echo ".."
+        sleep 1
+        echo "..."
+        sleep 1
+        echo "...."
 
-# 打印项目日志
-tail -f ${LOG_PATH}
+        is_exist
+        if [ $? -eq "0" ]; then
+            echo ">>> ${APPLICATION} is stoped FAIL"
+        else
+            echo ">>> ${APPLICATION} is stoped SUCCESS"
+        fi
+    else
+        echo ">>> ${APPLICATION} is not running"
+    fi
+}
 
+# 输出运行状态
+status() {
+    is_exist
+    if [ $? -eq "0" ]; then
+        echo ">>> ${APPLICATION} is running. pid is ${pid}"
+    else
+        echo ">>> ${APPLICATION} is NOT running."
+    fi
+}
+
+# 重启
+restart() {
+    stop
+    start
+}
+
+# 根据输入参数，选择执行对应方法，不输入则执行使用说明
+case "$1" in
+    "start")
+    start
+    ;;
+    "stop")
+    stop
+    ;;
+    "status")
+    status
+    ;;
+    "restart")
+    restart
+    ;;
+    *)
+    usage
+    ;;
+esac
